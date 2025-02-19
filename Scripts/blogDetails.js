2d7e85984cdbe060c6b006b342fef9ca344849b9
Abraham <abrahamedet9@gmail.com>
2025-02-19 14:38:25 +0100

    Share functionality in blog and copy link

--- Scripts/blogDetails.js
+++ Scripts/blogDetails.js
@@ -24,6 +24,41 @@ closeShareArticleBtn.addEventListener('click', ()=>{
   shareArticleShow()
 })
 
+document.addEventListener("DOMContentLoaded", function () {
+  const pageUrl = encodeURIComponent(window.location.href);
+  const pageTitle = encodeURIComponent(document.title);
+  //const source = encodeURIComponent("TalentMakers Foundation");
+
+  // Twitter Share Link
+  document.getElementById("shareTwitter").href = `https://x.com/intent/tweet?url=${pageUrl}&text=${pageTitle}`;
+
+  // Facebook Share Link
+  document.getElementById("shareFacebook").href = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`;
+
+  // LinkedIn Share Link
+  document.getElementById("shareLinkedIn").href = `https://www.linkedin.com/sharing/share-offsite/?url=${pageUrl}`;
+
+  //Link section 
+  document.getElementById("articleLink").textContent = pageUrl;
+});
+
+
+function copyText(textToCopy, copyBtn){
+  navigator.clipboard.writeText(textToCopy)
+  .then(()=>{
+      copyBtn.textContent = "Copied";
+  })
+  .catch(()=>{
+      const newError = new Error('Failed to copy text');
+      alert(newError) 
+  })
+}
+
+const copyBtn = document.getElementById("copyLink");
+copyBtn.addEventListener('click', ()=>{
+  const articleLink = document.getElementById("articleLink");
+  copyText(articleLink.textContent, copyBtn);
+})
 
 //Like article
 const LikeArticleForm = document.getElementById('likeArticleForm');
