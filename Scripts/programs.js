56323958f411391e848be96cce0fbbead7cbcc22
Abraham <abrahamedet9@gmail.com>
2025-02-21 14:02:40 +0100

    Programs page redesign

--- Scripts/programs.js
+++ Scripts/programs.js
@@ -1,3 +1,28 @@
+//Program Registration form popup
+
+const programSection = document.getElementById("progam-form-popup-section");
+const openProgramFormBtn = document.getElementById("open-program-form-btn");
+const openProgramFormBtn2 = document.getElementById("open-program-form-btn2");
+const openProgramFormBtn3 = document.getElementById("open-program-form-btn3");
+const closeProgramFormBtn = document.getElementById("close-program-form-popup");
+function showProgramForm() {
+  programSection.classList.toggle("hidden");
+  programSection.classList.toggle("flex");
+}
+openProgramFormBtn.addEventListener("click", () => {
+  showProgramForm();
+});
+openProgramFormBtn2.addEventListener("click", () => {
+  showProgramForm();
+});
+openProgramFormBtn3.addEventListener("click", () => {
+  showProgramForm();
+});
+closeProgramFormBtn.addEventListener("click", () => {
+  showProgramForm();
+});
+
+
 //Menu popup
 const menuSection = document.getElementById("mobileMenu");
 const menuBtn = document.getElementById("menu-btn");
