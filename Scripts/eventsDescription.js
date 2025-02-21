623db5c8b33f6a6899fd0961e6ea629055c37200
Abraham <abrahamedet9@gmail.com>
2025-02-21 14:52:05 +0100

    Share and social media links in Event details page

--- Scripts/eventsDescription.js
+++ Scripts/eventsDescription.js
@@ -35,3 +35,54 @@ function showTextArea() {
 checkBox.addEventListener("click", () => {
   showTextArea();
 });
+
+//Share article popup
+function shareArticleShow() {
+  const shareArticleSec = document.getElementById('share-article-section');
+  shareArticleSec.classList.toggle("hidden");
+  shareArticleSec.classList.toggle("flex");
+}
+const openShareArticleBtn = document.getElementById('open-Share-article-popup');
+const closeShareArticleBtn = document.getElementById('close-Share-article-popup');
+openShareArticleBtn.addEventListener('click', ()=>{
+  shareArticleShow()
+})
+closeShareArticleBtn.addEventListener('click', ()=>{
+  shareArticleShow()
+})
+
+document.addEventListener("DOMContentLoaded", function () {
+  const pageUrl = encodeURIComponent(window.location.href);
+  const pageTitle = encodeURIComponent(document.title);
+  //const source = encodeURIComponent("TalentMakers Foundation");
+
+  // Twitter Share Link
+  document.getElementById("shareTwitter").href = `https://x.com/intent/tweet?url=${pageUrl}&text=${pageTitle}`;
+
+  // Facebook Share Link
+  document.getElementById("shareFacebook").href = `https://www.facebook.com/sharer/sharer.php?u=${pageUrl}`;
+
+  // LinkedIn Share Link
+  document.getElementById("shareLinkedIn").href = `https://www.linkedin.com/sharing/share-offsite/?url=${pageUrl}`;
+
+  //Link section 
+  document.getElementById("articleLink").textContent = pageUrl;
+});
+
+
+function copyText(textToCopy, copyBtn){
+  navigator.clipboard.writeText(textToCopy)
+  .then(()=>{
+      copyBtn.textContent = "Copied";
+  })
+  .catch(()=>{
+      const newError = new Error('Failed to copy text');
+      alert(newError) 
+  })
+}
+
+const copyBtn = document.getElementById("copyLink");
+copyBtn.addEventListener('click', ()=>{
+  const articleLink = document.getElementById("articleLink");
+  copyText(articleLink.textContent, copyBtn);
+})
